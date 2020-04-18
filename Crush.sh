6693911669e47b86b95d3ad05315ce59949c4846
Sasori <63850356+Sonata7@users.noreply.github.com>
2020-04-18 09:25:06 +0000

    Create Crush.sh

---
+++ Crush.sh
@@ -0,0 +1,20 @@
+#!data/usr/bin/bash
+
+figlet -f big "Hey My Crush"
+echo "Enter your name"
+read name
+echo "Hello,$name"
+for loop in {1..100};do
+echo "#You are so cute $loop"
+done
+
+echo "Are you single?(only type yes or no)"
+read action
+echo "I am single"
+figlet -f small "Do you love me?"
+echo "(only type yes or no)"
+read action
+echo "I love you so much"
+for loop in {1..100};do
+echo "I love you so much $loop"
+done
